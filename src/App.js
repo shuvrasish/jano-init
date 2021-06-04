26e6d71d190c1ed13c9b4e3309598e0e6269b6b5
Shuvrasish Roy <shuvrasish24@gmail.com>
2021-06-05 00:55:24 +0530

    Add Notification Permission

--- src/App.js
+++ src/App.js
@@ -1,62 +1,123 @@
-import React from "react";
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import Cards from "./Cards";
-import 'firebase/analytics';
+import "firebase/analytics";
 import database from "./firebase";
-import firebase from "firebase/app"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-
+import firebase from "firebase/app";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import { IconButton } from "@material-ui/core";
 
 function App() {
-  
-  React.useEffect(() => {
-    let msg = null
+  console.log(Notification.permission);
+  const [notifPermission, setNotifPermission] = useState(
+    Notification.permission
+  );
+  // React.useEffect(() => {
+  //   let msg = null;
+  //   if (firebase.messaging.isSupported()) {
+  //     msg = firebase.messaging();
+  //     msg
+  //       .requestPermission()
+  //       .then(() => {
+  //         console.log();
+  //         return msg.getToken({
+  //           vapidKey:
+  //             "BNr6t72DsL2tuv-9_pcEhQqgQv-A1Irqpaohv5k48vC2Wg-Hf58bORmBK6g8adqT4hHOTC7IXaVr_Yjl1Vkh9Oc",
+  //         });
+  //       })
+  //       .then((data) => {
+  //         console.warn("token", data);
+
+  //         if (firebase.auth().currentUser == null) {
+  //           database
+  //             .collection("TokenWithoutLogin")
+  //             .doc("TokenWithoutLogin")
+  //             .update({
+  //               token: firebase.firestore.FieldValue.arrayUnion(data),
+  //             });
+  //         } else {
+  //           console.log(firebase.auth().currentUser.email);
+  //           database
+  //             .collection("Users")
+  //             .doc(firebase.auth().currentUser.email)
+  //             .update({
+  //               token: data,
+  //             });
+  //           database
+  //             .collection("TokenWithoutLogin")
+  //             .doc("TokenWithoutLogin")
+  //             .update({
+  //               token: firebase.firestore.FieldValue.arrayRemove(data),
+  //             });
+  //         }
+  //       });
+  //   } else {
+  //     //alert('no-support :(')
+  //   }
+
+  //   // const msg = firebase.messaging()
+  // });
+
+  //handler for allowing notifications.
+  //once notifications are allowed or blocked, the option can only be changed from the browser settings and the popup wont appear unless ask option is currently selected in the browser settings
+  const messagingNotificationHandler = () => {
+    let messaging = null;
     if (firebase.messaging.isSupported()) {
-      msg = firebase.messaging()
-      msg.requestPermission().then(() => {
-        console.log()
-        return msg.getToken({ vapidKey: "BNr6t72DsL2tuv-9_pcEhQqgQv-A1Irqpaohv5k48vC2Wg-Hf58bORmBK6g8adqT4hHOTC7IXaVr_Yjl1Vkh9Oc" });
-      }).then((data) => {
-        console.warn("token", data)
-
-        if (firebase.auth().currentUser == null) {
-          database.collection("TokenWithoutLogin").doc("TokenWithoutLogin").update(
-            {
-              token: firebase.firestore.FieldValue.arrayUnion(data),
-            }
-          )
-        }
-        else {
-          console.log(firebase.auth().currentUser.email)
-          database.collection("Users").doc(firebase.auth().currentUser.email).update(
-            {
-              token: data
-
-            }
-          )
-          database.collection("TokenWithoutLogin").doc("TokenWithoutLogin").update(
-            {
-              token: firebase.firestore.FieldValue.arrayRemove(data),
-            }
-          )
-        }
-      })
+      messaging = firebase.messaging();
+      //using "messaging" shows warning so replaced it with "Notification" which does the same thing
+      Notification.requestPermission()
+        .then((permission) => {
+          console.log(`Permission ${permission}`);
+          setNotifPermission(permission);
+          return messaging.getToken({
+            vapidKey:
+              "BNr6t72DsL2tuv-9_pcEhQqgQv-A1Irqpaohv5k48vC2Wg-Hf58bORmBK6g8adqT4hHOTC7IXaVr_Yjl1Vkh9Oc",
+          });
+        })
+        .then((data) => {
+          console.log(data);
+          if (firebase.auth().currentUser == null) {
+            database
+              .collection("TokenWithoutLogin")
+              .doc("TokenWithoutLogin")
+              .update({
+                token: firebase.firestore.FieldValue.arrayUnion(data),
+              });
+          } else {
+            database
+              .collection("Users")
+              .doc(firebase.auth().currentUser.email)
+              .update({
+                token: data,
+              });
+            database
+              .collection("TokenWithoutLogin")
+              .doc("TokenWithoutLogin")
+              .update({
+                token: firebase.firestore.FieldValue.arrayRemove(data),
+              });
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
     } else {
-      //alert('no-support :(')
+      console.log("Firebase Messaging not supported!");
     }
+  };
 
-    // const msg = firebase.messaging()
-
-
-
-  })
-  
   return (
     <div className="App">
+      <div className="notif-container">
+        {notifPermission === "default" ? (
+          <IconButton onClick={messagingNotificationHandler}>
+            <NotificationsActiveIcon fontSize="large" />
+          </IconButton>
+        ) : (
+          ""
+        )}
+      </div>
       <Router>
         <Switch>
           {/* <Route path="/card/:auth/:id">
--- src/Cards.js
+++ src/Cards.js
@@ -1,163 +1,178 @@
 import React from "react";
 import database from "./firebase";
 import "./Cards.css";
-import "./SwipeButtons.css"
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import CancelIcon from '@material-ui/icons/Cancel';
-import WhatsAppIcon from '@material-ui/icons/WhatsApp';
-import IconButton from '@material-ui/core/IconButton';
-import FeedbackIcon from '@material-ui/icons/Feedback';
-import LanguageIcon from '@material-ui/icons/Language';
-import firebase from "firebase/app"
-import 'firebase/analytics';
-import { Swipeable, direction } from 'react-deck-swiper';
+import "./SwipeButtons.css";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import CancelIcon from "@material-ui/icons/Cancel";
+import WhatsAppIcon from "@material-ui/icons/WhatsApp";
+import IconButton from "@material-ui/core/IconButton";
+import FeedbackIcon from "@material-ui/icons/Feedback";
+import LanguageIcon from "@material-ui/icons/Language";
+import firebase from "firebase/app";
+import "firebase/analytics";
+import { Swipeable, direction } from "react-deck-swiper";
 import { Helmet } from "react-helmet";
-import ShareIcon from '@material-ui/icons/Share';
-import MetaTags from 'react-meta-tags';
+import ShareIcon from "@material-ui/icons/Share";
+import MetaTags from "react-meta-tags";
 
 class Cards extends React.Component {
-    constructor(props) {
-        super(props);
-        this.withoutlogin = database.collection("CardsWithoutLogin");
-        this.withlogin = database.collection("CardsWithLogin").orderBy("sid", "asc");
-        this.users = database.collection("Users");
-        this.unsubscribe = null;
-        this.state = {
-            cards: [],
-            liked: [],
-            disliked: [],
-            totalshare: 0,
-            totalfeedback: 0,
-            totallikes: 0,
-            totaldislikes: 0
-        };
+  constructor(props) {
+    super(props);
+    this.withoutlogin = database.collection("CardsWithoutLogin");
+    this.withlogin = database
+      .collection("CardsWithLogin")
+      .orderBy("sid", "asc");
+    this.users = database.collection("Users");
+    this.unsubscribe = null;
+    this.state = {
+      cards: [],
+      liked: [],
+      disliked: [],
+      totalshare: 0,
+      totalfeedback: 0,
+      totallikes: 0,
+      totaldislikes: 0,
+    };
+  }
 
-    }
+  componentDidMount() {
+    this.unsubscribe = this.withoutlogin.onSnapshot(this.onCollectionUpdate);
+  }
 
-    componentDidMount() {
+  onCollectionUpdate = (querySnapshot) => {
+    const cards = [];
 
-        this.unsubscribe = this.withoutlogin.onSnapshot(this.onCollectionUpdate)
-
-    }
-
-
-
-    onCollectionUpdate = (querySnapshot) => {
-        const cards = [];
-
-        querySnapshot.forEach((doc) => {
-            const { categories, heading, id, image_links, main_category, ns, pageid, reference, sid, subheading, summary, webpage_url } = doc.data();
-            if (!(this.state.liked.includes(id) || this.state.disliked.includes(id))) {
-                cards.push({
-                    key: doc.id,
-                    doc, // DocumentSnapshot
-                    categories,
-                    heading,
-                    id,
-                    image_links,
-                    main_category,
-                    ns,
-                    pageid,
-                    reference,
-                    sid,
-                    subheading,
-                    summary,
-                    webpage_url,
-                });
-            }
-        })
-        //console.log("Cards on screen: "+cards.length)
-        this.setState({
-            cards: [...this.state.cards, ...cards.slice(0, 5)]
+    querySnapshot.forEach((doc) => {
+      const {
+        categories,
+        heading,
+        id,
+        image_links,
+        main_category,
+        ns,
+        pageid,
+        reference,
+        sid,
+        subheading,
+        summary,
+        webpage_url,
+      } = doc.data();
+      if (
+        !(this.state.liked.includes(id) || this.state.disliked.includes(id))
+      ) {
+        cards.push({
+          key: doc.id,
+          doc, // DocumentSnapshot
+          categories,
+          heading,
+          id,
+          image_links,
+          main_category,
+          ns,
+          pageid,
+          reference,
+          sid,
+          subheading,
+          summary,
+          webpage_url,
         });
-        console.log("Cards on screen: " + this.state.cards.length)
-        //this.cardsanalytics()
+      }
+    });
+    //console.log("Cards on screen: "+cards.length)
+    this.setState({
+      cards: [...this.state.cards, ...cards.slice(0, 5)],
+    });
+    console.log("Cards on screen: " + this.state.cards.length);
+    //this.cardsanalytics()
+  };
 
+  swiped = (swipeDirection) => {
+    var arry = this.state.cards;
+    var cardid = arry[arry.length - 1].id;
 
-    }
+    console.log(cardid + " swiped " + swipeDirection);
 
+    arry.pop();
+    this.setState({ cards: arry });
+  };
 
+  render() {
+    return (
+      <div className="App">
+        {this.state.cards.map((cards) => (
+          <div key={cards.id} id={cards.id}>
+            <MetaTags>
+              <title>{cards.heading}</title>
+              <meta name="description" content={cards.subheading} />
+            </MetaTags>
+            <Swipeable
+              onSwipe={this.swiped}
+              renderButtons={({ left, right }) => (
+                <div className="btn">
+                  <div className="swipeButtons">
+                    <IconButton
+                      className="swipeButtons_rep"
+                      href="https://docs.google.com/forms/u/2/d/e/1FAIpQLSdzFp8LdIP4cOHMs6mMSXXALHm1n-DXo1HHxQPBM8SYX96n7Q/viewform?embedded=true"
+                      onClick={this.feedback}
+                    >
+                      <FeedbackIcon fontSize="large" />
+                    </IconButton>
 
+                    <IconButton className="swipeButtons_canc" onClick={left}>
+                      <CancelIcon fontSize="large" />
+                    </IconButton>
+                    <IconButton
+                      className="swipeButtons_web"
+                      onClick={this.handleWeb}
+                    >
+                      <LanguageIcon fontSize="large" />
+                    </IconButton>
+                    <IconButton className="swipeButtons_fav" onClick={right}>
+                      <FavoriteIcon fontSize="large" />
+                    </IconButton>
+                    <IconButton
+                      className="swipeButtons_what"
+                      onClick={this.handleShare}
+                      data-action="share/whatsapp/share"
+                    >
+                      <ShareIcon fontSize="large" />
+                    </IconButton>
+                  </div>
+                </div>
+              )}
+            >
+              <div className="card">
+                <div className="buttonFalse">
+                  <img src={cards.image_links[0]} alt={cards.heading} />
+                </div>
+                <div className="post-content">
+                  <div className="category">
+                    <mark className="highlight">
+                      {" "}
+                      {"  #" + cards.main_category + "  "}{" "}
+                    </mark>
+                  </div>
+                  <h1 className="title">{cards.heading}</h1>
+                  <div className="sub_title">
+                    <p>{cards.subheading}</p>
+                  </div>
 
+                  <hr className="solid"></hr>
 
-    swiped = (swipeDirection) => {
-        var arry = this.state.cards
-        var cardid = arry[arry.length - 1].id
-
-        console.log(cardid + " swiped " + swipeDirection)
-
-
-        arry.pop()
-        this.setState({ cards: arry })
-
-    }
-
-
-
-
-
-
-
-    render() {
-        return (
-            <div className="App">
-                {this.state.cards.map((cards) =>
-                    <div key={cards.id} id={cards.id} >
-
-                        <MetaTags>
-                            <title>{cards.heading}</title>
-                            <meta name="description" content={cards.subheading} />
-                        </MetaTags>
-                        <Swipeable
-                            onSwipe={this.swiped}
-                            renderButtons={({ left, right }) => (
-                                <div className="btn">
-                                    <div className="swipeButtons">
-                                        <IconButton className="swipeButtons_rep" href="https://docs.google.com/forms/u/2/d/e/1FAIpQLSdzFp8LdIP4cOHMs6mMSXXALHm1n-DXo1HHxQPBM8SYX96n7Q/viewform?embedded=true" onClick={this.feedback} >
-                                            <FeedbackIcon fontSize="large" />
-                                        </IconButton>
-
-                                        <IconButton className="swipeButtons_canc" onClick={left}>
-                                            <CancelIcon fontSize="large" />
-                                        </IconButton>
-                                        <IconButton className="swipeButtons_web" onClick={this.handleWeb} >
-                                            <LanguageIcon fontSize="large" />
-                                        </IconButton>
-                                        <IconButton className="swipeButtons_fav" onClick={right} >
-                                            <FavoriteIcon fontSize="large" />
-                                        </IconButton>
-                                        <IconButton className="swipeButtons_what" onClick={this.handleShare} data-action="share/whatsapp/share">
-                                            <ShareIcon fontSize="large" />
-                                        </IconButton>
-                                    </div>
-                                </div>
-                            )}
-                        >
-                            <div className="card"  >
-                                <div className="buttonFalse">
-                                    <img src={cards.image_links[0]} alt={cards.heading} />
-                                </div>
-                                <div className="post-content">
-                                    <div className="category"><mark className="highlight"> {"  #" + cards.main_category + "  "} </mark></div>
-                                    <h1 className="title">{cards.heading}</h1>
-                                    <div className="sub_title">
-                                        <p>{cards.subheading}</p>
-                                    </div>
-
-                                    <hr className="solid"></hr>
-
-                                    <p className="descriptionn">{cards.summary.replace(/@n/gi, '\n\n•')}<br /><br /><b>{cards.reference}</b></p>
-
-                                </div>
-                            </div>
-
-                        </Swipeable>
-
-                    </div>
-                )}
-            </div>
-        );
-    }
+                  <p className="descriptionn">
+                    {cards.summary.replace(/@n/gi, "\n\n•")}
+                    <br />
+                    <br />
+                    <b>{cards.reference}</b>
+                  </p>
+                </div>
+              </div>
+            </Swipeable>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Cards;
